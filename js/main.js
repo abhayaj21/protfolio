58a5136a4fbf77022ef30bf08a836c89fb4aa3c9
abhayaj21 <abhayaj2121@gmail.com>
2026-02-13 20:35:09 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//them change
 const light = document.getElementById("light");
 const dark = document.getElementById("dark");
 const theme = document.getElementById("theme");
@@ -10,3 +12,26 @@ mod.addEventListener("change", () => {
     theme.href = "";
   }
 });
+
+//set active
+
+// function setActive()
+// {
+//   const navbar = document.getElementById("navbar");
+//   const a_tags = navbar.getElementsByTagName('a');
+//   let path = document.location.href.split("/").pop().split("#").pop();
+//   console.log(path);
+//   for(let i = 0; i < a_tags.length; i++)
+//   {
+//     let file = a_tags[i].href.split("/").pop();
+//     file_name = file.split("#").pop();
+
+//     if(path == file_name)
+//     {
+//       a_tags[i].classList.add("active");
+//       document.getElementById("first-child").classList.remove('active');
+//     }
+//   }
+// }
+
+// setInterval(setActive,1000);
